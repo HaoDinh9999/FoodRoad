3ef1138815f8fab71faeceb5113228aab8eec4d0
Hào Đinh Việt <vhao1509@gmail.com>
2021-10-18 22:38:39 +0700

    Add gitignore

--- src/components/herosection/HeroSection.js
+++ src/components/herosection/HeroSection.js
@@ -2,13 +2,33 @@ import React from 'react';
 import '../../App.css';
 import { Button } from '../button/Button';
 import './HeroSection.css';
+import Typewriter from 'typewriter-effect';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='videos/video-dinner.mp4' autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-     
+      <h1> 
+          <Typewriter
+        options={{
+    strings: [' Spreading Happiness', ' Connecting People'," Creating Memories"," Creating Stories"," Fulfilling Adventure"],
+    autoStart: true,
+    loop: true,
+  }}
+  onInit={(typewriter) => {
+    typewriter.typeString()
+      .callFunction(() => {
+        console.log('String typed out!');
+      })
+      .pauseFor(1000)
+      .deleteAll()
+      .callFunction(() => {
+        console.log('All strings were deleted');
+      })
+      .start();
+  }}
+/>
+</h1>
       <div className='hero-btns'>
         <Button
           className='btns'
