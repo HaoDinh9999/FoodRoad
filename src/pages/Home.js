3a077e9f04f0c472b5ea68264cc9f21aac6e77d7
feelings93 <19521298@gm.uit.edu.vn>
2021-10-25 23:29:40 +0700

    Make some hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import '../App.css';
-import Footer from '../components/footer/Footer';
-import HeroSection from '../components/layoutHome/herosection/HeroSection';
-import AllOurServices from '../components/layoutHome/ourservice/AllOurServices';
-import AllSales from '../components/layoutHome/sale/AllSales';
-import AllCardTours from '../components/layoutHome/cardtour/AllCardTours';
-import AllNewsReview from '../components/layoutHome/newsreview/AllNewsReview';
+import React from "react";
+import "../App.css";
+import Footer from "../components/footer/Footer";
+import HeroSection from "../components/layoutHome/herosection/HeroSection";
+import AllOurServices from "../components/layoutHome/ourservice/AllOurServices";
+import AllSales from "../components/layoutHome/sale/AllSales";
+import AllCardTours from "../components/layoutHome/cardtour/AllCardTours";
+import AllNewsReview from "../components/layoutHome/newsreview/AllNewsReview";
 function Home() {
   return (
     <>
-      <HeroSection ></HeroSection>
+      <HeroSection></HeroSection>
       <AllOurServices></AllOurServices>
       <AllSales></AllSales>
       <AllCardTours></AllCardTours>
