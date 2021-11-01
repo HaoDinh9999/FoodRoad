5f8b6d6efb3ee8648041390accb6c57968170137
Al3xDo <56794124+Al3xDo@users.noreply.github.com>
2021-11-01 16:30:55 +0700

    update card

--- src/components/card/CardItem.js
+++ src/components/card/CardItem.js
@@ -1,25 +1,123 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 
-function CardItem(props) {
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { Button, Grid } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Favorite from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import { Checkbox } from '@mui/material';
+import { blue, pink } from '@mui/material/colors';
+import "./carditem.css"
+import { Box } from '@mui/system';
+
+const BestSellerMedia =(props) => {
   return (
     <>
-      <li className='cards__item'>
-        <Link className='cards__item__link' to={props.path}>
-          <figure className='cards__item__pic-wrap' data-category={props.label}>
-            <img
-              className='cards__item__img'
-              alt="images/img-1.jpg"
-              src={props.src}
-            />
-          </figure>
-          <div className='cards__item__info'>
-            <h5 className='cards__item__text'>{props.text}</h5>
-          </div>
-        </Link>
-      </li>
+    <Box sx={{ position: 'relative' }}>
+      <CardMedia
+        component="img"
+        height="194"
+        image={props.src}
+        alt="tour image"
+        className="media"
+      />
+      <Box
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left:212,
+            width: '100%',
+            // bgcolor: 'rgba(0, 0, 0, 0.54)',
+            color: 'white',
+          }}
+        >
+        <img src="/images/cards/bestseller_tag.png"/>
+          {/* <Typography variant="body2">Subtitle</Typography> */}
+        </Box>
+    </Box>
     </>
+  )
+}
+const NormalMedia =(props) => {
+  return (
+    <CardMedia
+        component="img"
+        height="194"
+        image={props.src}
+        alt="tour image"
+        className="media"
+      />
+  )
+}
+const showMedia = (isBestSeller, src) => {
+  if (isBestSeller)
+  {
+    return <NormalMedia src={src}/>;
+  } else
+  {
+    return <BestSellerMedia src={src}/>
+  }
+}
+export default function CardItem(props) {
+  const {src,title, content, isBestSeller, timeStart, timeEnd, price, concurency}= props
+  return (
+      <Card sx={{ maxWidth: 380}} className="card">
+      {showMedia(isBestSeller, src)}
+      <CardHeader title={title} xs= {{pb: 0}} className= "text-uppercase"/>
+      <CardContent sx={{pt: 0, pb:0}}>
+      <Grid container  className= "border-btm">
+          <Grid item xs={9}>
+            <Typography variant="subtitle2">
+            <AccessAlarmIcon/>
+                {timeEnd} - {timeStart}
+            </Typography>
+          </Grid>
+          <Grid item >
+            <Typography variant="h6" className="price-txt">
+                {price} {concurency}
+            </Typography>  
+          </Grid>
+          </Grid>
+        <Typography variant="body1" color="text.secondary">
+        {content}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        {/* <IconButton aria-label="add to favorites" className="favourite-icon">
+          <FavoriteIcon />
+        </IconButton> */}
+        {/* <IconButton aria-label="share" className="share-icon">
+          <ShareIcon  />
+        </IconButton> */}
+        <Checkbox  icon={<FavoriteBorder />} checkedIcon={<Favorite />} 
+        sx={{
+          color: pink[800],
+          '&.Mui-checked': {
+            color: pink[600],
+             },
+            }}/>
+        <Checkbox icon={<BookmarkBorderIcon />} checkedIcon={<BookmarkIcon />} 
+          sx={{
+          color: blue[800],
+          '&.Mui-checked': {
+            color: blue[600],
+             },
+            }}
+        />
+        <Button className="book-now-btn" variant="contained" color="error">
+            Book Now
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
-
-export default CardItem;
--- src/components/card/Cards.js
+++ src/components/card/Cards.js
@@ -1,51 +1,157 @@
-import React from 'react';
-import './Cards.css';
-import CardItem from './CardItem';
-
-function Cards() {
+import React, { useState } from 'react';
+// import CardItem from "./CardItem"
+import CardItem from "./CardItem"
+import {FormControlLabel, FormLabel, Grid, FormGroup, FormControl, FormHelperText, Container} from "@mui/material"
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import Checkbox from '@mui/material/Checkbox';
+import { useEffect } from 'react';
+import ListIcon from '@mui/icons-material/List';
+import { Collapse } from '@mui/material';
+import {data} from './data';
+import { IconButton } from '@mui/material';
+export default function Cards() {
+	const [searchWord, setSearchWord]= useState("")
+	const [cardList, setCardList] = useState([])
+	const [isChecked, setIsChecked] = useState(false)
+	useEffect(() => {
+		var res= data.map((item, index) => {
+			const {src,title, content, isBestSeller, timeStart, timeEnd, price, concurency, id}= item
+			  return (
+				<Grid item xs={4} key= {index}>
+					<CardItem
+					  id= {id}
+					  src= {src}
+					  title= {title}
+					  content={content}
+					  isBestSeller={isBestSeller}
+					  timeStart={timeStart}
+					  timeEnd= {timeEnd}
+					  price={price}
+					  concurency={concurency}
+					/>
+				</Grid>
+			  )
+			})
+		  setCardList(res)
+	}, []);
+	const onChange =(event) => {
+		setSearchWord(event.target.value)
+		if (searchWord === null || searchWord === "" || searchWord.trim().length <= 2)
+		{
+			var res= data.map((item, index) => {
+				const {src,title, content, isBestSeller, timeStart, timeEnd, price, concurency, id}= item
+				  return (
+					<Grid item xs={4} key= {index}>
+						<CardItem
+						  id= {id}
+						  src= {src}
+						  title= {title}
+						  content={content}
+						  isBestSeller={isBestSeller}
+						  timeStart={timeStart}
+						  timeEnd= {timeEnd}
+						  price={price}
+						  concurency={concurency}
+						/>
+					</Grid>
+				  )
+				})
+			  setCardList(res)
+		} 
+		else
+		{
+			var filtered= data.filter((item) => (item.title.toLowerCase().indexOf(searchWord.toLowerCase()) !== -1));
+			var res= filtered.map((item, index) => {
+				const {src,title, content, isBestSeller, timeStart, timeEnd, price, concurency, id}= item
+				  return (
+					<Grid item xs={4} key= {index}>
+						<CardItem
+						  id= {id}
+						  src= {src}
+						  title= {title}
+						  content={content}
+						  isBestSeller={isBestSeller}
+						  timeStart={timeStart}
+						  timeEnd= {timeEnd}
+						  price={price}
+						  concurency={concurency}
+						/>
+					</Grid>
+				  )
+				})
+			  setCardList(res)
+		}
+	}
   return (
-    <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/img-1.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
-              path='/services'
-            />
-            <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
-            />
-          </ul>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
-              path='/services'
-            />
-            <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
-              path='/products'
-            />
-            <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
-            />
-          </ul>
-        </div>
-      </div>
+    <div className="container">
+    <Grid container  spacing={95}>
+    <Grid item>
+    <Breadcrumbs aria-label="breadcrumb" sx={{pb:3, pt:3}}>
+        <Link underline="hover" color="inherit" href="/">
+          Home
+        </Link>
+      	<Typography color="text.primary">Tours</Typography>
+    </Breadcrumbs>
+      </Grid>
+      <Grid item>
+      <Box sx={{ display: 'flex', alignItems: 'flex-end', pb:3, pt:3 }}>
+        <SearchIcon sx={{ color: 'action.active' }} />
+        <TextField 
+		id="input-with-sx" 
+		label="Seach" 
+		variant="standard" 
+		sx={{width:'36ch'}}
+		onChange={(event) => {onChange(event)}}
+		value={searchWord}
+		name="searchWord"
+		/>
+		<IconButton onClick={() => {setIsChecked(!isChecked)}}>
+		<ListIcon sx={{fontSize:30}}/>
+		</IconButton>
+      </Box>
+      </Grid>
+    </Grid>
+	<Collapse in={isChecked} sx={{pb:2}}>
+	<Box sx={{ display: 'flex' , pt:0}}>
+      <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+	<Typography variant="subtitle2"> Advanced Search</Typography>
+        <FormGroup sx={{display: 'block'}}>
+          <FormControlLabel
+            control={
+              <Checkbox  name="gilad" />
+            }
+            label="Vegan"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox  name="jason" />
+            }
+            label="Bear"
+          />
+		  <FormControlLabel
+            control={
+              <Checkbox  name="gilad" />
+            }
+            label="Day"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox  name="jason" />
+            }
+            label="Night"
+          />
+        </FormGroup>
+      </FormControl>
+    </Box>
+        </Collapse>
+  <Grid container  spacing={5}>
+    {cardList}
+  </Grid>
     </div>
   );
 }
-
-export default Cards;
