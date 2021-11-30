747820fd566d754558e28c874e02f5eb78d1fdbc
Hào Đinh Việt <vhao1509@gmail.com>
2021-11-30 16:27:28 +0700

    Update nhieu cho

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,11 +4,17 @@ import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import Footer from "../components/footer/Footer";
 import CardHero from "../components/layoutProfile/CardHero";
-
+import Aos from "aos";
+import "aos/dist/aos.css";
 const Profile = () => {
+  React.useEffect(() => {
+    Aos.init();
+    Aos.refresh();
+  }, []);
   return (
-    <CardHero></CardHero>
-    // <h1>a</h1>
+    <div data-aos="fade-up" data-aos-duration={900}>
+      <CardHero></CardHero>
+    </div>
   );
 };
 export default Profile;
